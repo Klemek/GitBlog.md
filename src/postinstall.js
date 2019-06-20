7710eaad252a7f997fa34ef8128f250aadf01ab6
Clément GOUIN <clement.gouin@thalesgroup.com>
2019-06-20 09:06:47 +0200

    Fixed wrong date on postinstall.js

--- src/postinstall.js
+++ src/postinstall.js
@@ -12,17 +12,18 @@ const copy = (src,dest) => {
 
 copy('./src/config.default.json','./config.example.json');
 
-if (!fs.existsSync('./data'))
+if (!fs.existsSync('./data')) {
     fs.mkdirSync('./data');
 
-copy('./sample_data/home','./data');
+    copy('./sample_data/home','./data');
 
-const pad0 = n => ('0'+n).substr(-2);
+    const pad0 = n => ('0'+n).substr(-2);
 
-const datetime = new Date();
-const dir = `./data/${datetime.getFullYear()}/${pad0(datetime.getMonth())}/${pad0(datetime.getDay())}`;
+    const datetime = new Date();
+    const dir = `./data/${datetime.getFullYear()}/${pad0(datetime.getMonth()+1)}/${pad0(datetime.getDate())}`;
 
-if (!fs.existsSync(dir))
-    fs.mkdirSync(dir, {recursive: true});
+    if (!fs.existsSync(dir))
+        fs.mkdirSync(dir, {recursive: true});
 
-copy('./sample_data/article',dir);
+    copy('./sample_data/article',dir);
+}
